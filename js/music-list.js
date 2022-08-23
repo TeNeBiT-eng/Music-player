d29430be4e5994947dd5e1ef722af00d6470835b
Tomisin Babalola <ademolababalola2003@gmail.com>
2022-08-23 12:00:36 +0100

    ...

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Abraham Akatu - Ogbenjuwa",
+        artist: "Abraham Akatu",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Harley Bird - Home",
+        artist: "Jordan Schor",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Harley Bird - Home",
+        artist: "Jordan Schor",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Harley Bird - Home",
+        artist: "Jordan Schor",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Harley Bird - Home",
+        artist: "Jordan Schor",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Harley Bird - Home",
+        artist: "Jordan Schor",
+        img: "music-6",
+        src: "music-6"
+    },
+]
